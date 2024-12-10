947355f7d538ab0b7a69e96cb61fc572945d8e0e
James Terrell <terrellj000@gmail.com>
2024-12-10 01:42:30 -0500

    have a full game running with numba functions. Its blazing fast. Need to code common sense filtering for branches now.

--- n_play_round.py
+++ n_play_round.py
@@ -42,3 +42,37 @@ def round1(hands_dealt):
     print(branch_count)
     # Trim to actual size
     return r2_leads[:branch_count], r2_hands[:branch_count, :, :, :]
+
+@njit
+def next_round(current_hands, leads, game_round, game_score):
+    # Pre-allocate maximum possible size
+    max_branches = len(leads)*4*4*4*4  # Worst-case scenario
+    next_round_hands = np.zeros(shape=(max_branches, current_hands.shape[1], current_hands.shape[2]-1, current_hands.shape[3]), dtype=np.int64)
+    next_round_leads= np.zeros(max_branches, dtype=np.int64)
+    total_score = np.zeros(shape=(max_branches, game_round, 1), dtype=np.int64)
+    possible_play = 5-game_round+1
+    # Track actual number of branches
+    branch_count = 0
+
+    for hand, lead, score in zip(current_hands, leads, game_score):
+        for card in range(possible_play):
+            branch = n_play_round(hands=hand, lead=lead, card_play=card)
+            for i in branch:
+                hand_set = array_set_difference(hand, i)
+                next_round_hands[branch_count, :, :, :] = hand_set
+                rd_winner = n_find_winner(trick=i, lead=lead)
+                next_round_leads[branch_count] = rd_winner
+                # try:
+                # for j in range(len(total_score[branch_count])-1):
+                #     total_score[branch_count, j] = score
+                # except:
+                for j in range(game_round-1):
+                    total_score[branch_count, j] = score[j]
+
+                
+                total_score[branch_count, game_round-1] = rd_winner
+                
+                branch_count += 1
+    print(branch_count)
+    # Trim to actual size
+    return next_round_leads[:branch_count], next_round_hands[:branch_count, :, :, :], total_score[:branch_count]
